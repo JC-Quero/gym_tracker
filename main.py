a02329cb579f371f37dd2f5bf370d5e140170c7c
Julio Correa <jccq.2003@gmail.com>
2025-12-19 17:02:38 -0300

    Fix: Habilitar CORS para vercel

--- main.py
+++ main.py
@@ -14,13 +14,11 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
-origins = [
-    "http://localhost:3000",
-]
+origins = ["*"] 
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=origins, 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
