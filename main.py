80fae2c68e7db44210ef11a4dbfa022d02b97a6c
Julio Correa <jccq.2003@gmail.com>
2025-12-20 10:51:23 -0300

    Feat: devolver user_id en login

--- main.py
+++ main.py
@@ -150,4 +150,9 @@ def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db:
         data={"sub": user.username, "id": user.id, "role": user.role}
     )
 
-    return {"access_token": access_token, "token_type": "bearer"}
+    return {
+        "access_token": access_token, 
+        "token_type": "bearer",
+        "user_id": user.id,       
+        "username": user.username 
+    }
